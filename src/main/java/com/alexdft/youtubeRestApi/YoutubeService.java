f3e8bc8f5c77651d40907bc7c6abf6f7bdc3aa16
alexdft7 <64974493+alexdft7@users.noreply.github.com>
2020-05-08 23:09:53 +0300

    Update YoutubeService.java

--- src/main/java/com/alexdft/youtubeRestApi/YoutubeService.java
+++ src/main/java/com/alexdft/youtubeRestApi/YoutubeService.java
@@ -1,7 +1,7 @@
 package com.alexdft.youtubeRestApi;
 
-import com.alexdft.youtubeRestApi.item.YouTubeCommentItem;
-import com.alexdft.youtubeRestApi.item.YouTubeSearchItem;
+import com.alexdft.youtubeRestApi.item.YoutubeCommentsItem;
+import com.alexdft.youtubeRestApi.item.YoutubeSearchItem;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.HttpTransport;
@@ -28,15 +28,15 @@ public class YoutubeService {
 
     private static final String YOUTUBE_SEARCH_URL = "https://www.youtube.com/watch?v=";
 
-    private ResourceBundle propertiesBundle = ResourceBundle.getBundle("youtube");
+    private ResourceBundle propertiesBundle = ResourceBundle.getBundle("YouTubeApp");
 
-    YouTube youTube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
-        @Override
-        public void initialize(HttpRequest httpRequest) throws IOException {
-        }
-    }).build();
+    public List<YoutubeCommentsItem> getYouTubeComments(String commentId) throws IOException {
 
-    public List<YouTubeCommentItem> getYouTubeComments(String commentId) throws IOException {
+        YouTube youTube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
+            @Override
+            public void initialize(HttpRequest httpRequest) {
+            }
+        }).build();
 
         YouTube.Comments.List search = youTube.comments().list("snippet");
 
@@ -44,14 +44,14 @@ public class YoutubeService {
         search.setFields("items(id,snippet/authorDisplayName,snippet/textOriginal,snippet/likeCount,snippet/publishedAt)");
         search.setId(commentId);
 
-        List<YouTubeCommentItem> result = new ArrayList<>();
+        List<YoutubeCommentsItem> result = new ArrayList<>();
 
         CommentListResponse response = search.execute();
         List<Comment> searchResultList = response.getItems();
 
         if (searchResultList != null && searchResultList.size() > 0) {
             for (Comment searchResult : searchResultList) {
-                YouTubeCommentItem item = new YouTubeCommentItem(
+                YoutubeCommentsItem item = new YoutubeCommentsItem(
                         searchResult.getSnippet().getAuthorDisplayName(),
                         searchResult.getSnippet().getTextOriginal(),
                         searchResult.getSnippet().getLikeCount().toString(),
@@ -62,7 +62,13 @@ public class YoutubeService {
         return result;
     }
 
-    public List<YouTubeSearchItem> getYoutubeSearch(String searchQuery) throws IOException {
+    public List<YoutubeSearchItem> getYoutubeSearch(String searchQuery) throws IOException {
+
+        YouTube youTube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
+            @Override
+            public void initialize(HttpRequest httpRequest) throws IOException {
+            }
+        }).build();
 
         YouTube.Search.List search = youTube.search().list("id,snippet");
 
@@ -70,7 +76,7 @@ public class YoutubeService {
         search.setFields("items(id/kind,id/videoId,snippet/title,snippet/description,snippet/thumbnails/default/url)");
         search.setQ(searchQuery);
 
-        List<YouTubeSearchItem> result = new ArrayList<>();
+        List<YoutubeSearchItem> result = new ArrayList<>();
 
         SearchListResponse searchResponse = search.execute();
         List<SearchResult> searchResultList = searchResponse.getItems();
@@ -78,7 +84,7 @@ public class YoutubeService {
         if (searchResultList != null && searchResultList.size() > 0) {
 
             for (SearchResult searchResult : searchResultList) {
-                YouTubeSearchItem item = new YouTubeSearchItem(
+                YoutubeSearchItem item = new YoutubeSearchItem(
                         YOUTUBE_SEARCH_URL + searchResult.getId().getVideoId(),
                         searchResult.getSnippet().getTitle(),
                         searchResult.getSnippet().getThumbnails().getDefault().getUrl(),
