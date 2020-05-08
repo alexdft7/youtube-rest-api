64d8ccfe837058fd4f886fb2f71304f0c7813264
alexdft7 <64974493+alexdft7@users.noreply.github.com>
2020-05-08 23:48:17 +0300

    Update AppConfig.java

--- src/main/java/com/alexdft/youtubeRestApi/configuration/AppConfig.java
+++ src/main/java/com/alexdft/youtubeRestApi/configuration/AppConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan(basePackages = "com.alexdft.youtubeRestApi.configuration")
+@ComponentScan(basePackages = "com.alexdft.youtubeRestApi")
 @EnableWebMvc
 public class AppConfig {
 }
